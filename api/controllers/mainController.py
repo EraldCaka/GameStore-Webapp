15cc3cc456755f80e7aac2479f0b76e593b8185f
EraldCaka <eraldcaka47@gmail.com>
2023-04-02 19:22:39 +0200

    main controller change

--- api/controllers/mainController.py
+++ api/controllers/mainController.py
@@ -6,7 +6,7 @@ from controllers.validation import  loginController, registerController
 
 userModel.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Users API", version="0.1.0")
+app = FastAPI(title="GameStore API", version="0.1.0")
 
 #user controller router
 app.include_router(userController.router)
